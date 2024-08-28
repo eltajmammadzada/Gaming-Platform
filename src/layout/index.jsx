0b5e67ef5a8cad8047f73285f286f35b408e646c
Eltaj Mammadzada <eltacmemmedzade@gmail.com>
2024-08-28 16:03:49 +0400

    layout and folder sturcture

---
+++ src/layout/index.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import {Outlet} from 'react-router-dom'
+
+
+export default function MainLayout() {
+  return (
+    <>
+     <Header />
+     <Outlet />
+     <Footer />
+    </>
+  )
+}
