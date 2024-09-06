6e8d30238409aee0b12201faa6290d7894155871
Eltaj Mammadzada <eltacmemmedzade@gmail.com>
2024-09-06 19:16:50 +0400

    mainddd

--- src/pages/ContactUs/index.jsx
+++ src/pages/ContactUs/index.jsx
@@ -1,3 +1,15 @@
 export default function ContactUs() {
- return <div>ContactUs</div>
+ return (
+    <div>
+
+        <div className="text-center flex flex-col gap-5">
+           <div>
+           <span>Home</span><span>/</span><span>Contact us</span>
+           </div>
+            <h1>Lorem Ipsum is simply dummy text of the printing and.</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+            <img src="/home/contact.png" alt="" />
+        </div>
+    </div>
+ )
 }
