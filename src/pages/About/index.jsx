18529d3f0b7128e1a37c317c1880062c2b31728d
Eltaj Mammadzada <eltacmemmedzade@gmail.com>
2024-09-06 19:18:09 +0400

    delete file

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,8 +1,6 @@
 import CardHeader from './WorkSections/CardHeader'
-import CardsItems from './WorkSections/Cards/CardsItems'
 import Hero from './abotSections/Hero'
 import Cards from './WorkSections/Cards/cards'
-import ImageLeft from './ThirdSections/CardSection/ImageLeft'
 import CardSection from './ThirdSections/CardSection'
 
 export default function About() {
