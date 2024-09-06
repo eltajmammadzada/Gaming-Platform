90b9d3e549ed325aaa5d19170357c06d2bf2f973
Eltaj Mammadzada <eltacmemmedzade@gmail.com>
2024-09-06 19:27:58 +0400

    cardsss

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,6 +1,6 @@
 import CardHeader from './WorkSections/CardHeader'
 import Hero from './abotSections/Hero'
-import Cards from './WorkSections/Cards/cards'
+import Cards from './WorkSections/Cards/Cards'
 import CardSection from './ThirdSections/CardSection'
 
 export default function About() {
